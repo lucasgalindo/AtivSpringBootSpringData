737a971cfe4387ac941fab7357c40b4bc82bd50f
Lucas Galindo <lucasgalindomds@gmail.com>
2024-09-23 13:33:43 -0300

    SpringBoot + SpringData

---
+++ src/main/java/com/example/SpringBootSpringData/SpringBootSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootSpringData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpringDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootSpringData/SpringBootSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootSpringData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
